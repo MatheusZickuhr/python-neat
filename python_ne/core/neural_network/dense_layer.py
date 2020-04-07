7caf46de5c3e80d67e75c21ddf2ddef9fdedd72a
MatheusZickuhr <matheus.zickuhr@edu.udesc.br>
2020-04-07 13:11:14 -0300

    matrix dot product optimizations

--- python_ne/core/neural_network/dense_layer.py
+++ python_ne/core/neural_network/dense_layer.py
@@ -22,26 +22,9 @@ class DenseLayer:
         self.weights = np.array([[random.uniform(-1, 1) for j in range(self.units)] for i in range(self.input_shape[0])])
         self.initialized = True
 
-    def feedforward(self, x_list):
-        output = np.zeros(self.units)
-        weighted_xs = np.zeros((len(x_list), self.units))
-
-        i_len = len(x_list)
-        j_len = self.units
-
-        for i in range(i_len):
-            for j in range(j_len):
-                weighted_xs[i][j] = (
-                        self.weights[i][j] * x_list[i]
-                )
-
-        for j in range(j_len):
-            weighted_sum = 0
-            for i in range(i_len):
-                weighted_sum += weighted_xs[i][j]
-            output[j] = self.activation(weighted_sum + self.bias[j])
-
-        return output
+    def feedforward(self, xs):
+        output = xs.dot(self.weights)
+        return self.activation(output + self.bias)
 
     def get_weights(self):
         return copy.copy(self.weights), copy.copy(self.bias)
